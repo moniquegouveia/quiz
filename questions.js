ad957916868a5f5fc364564161a931f01b292c4d
moniquegouveia <90391598+moniquegouveia@users.noreply.github.com>
2022-02-17 09:17:38 +0000

    insert quiz questions

---
+++ questions.js
@@ -0,0 +1,80 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which of the following is NOT a valid border-style property value?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+ {
+ numb: 6,
+question: "There are 16 basic color keywords in CSS. Which of the following are NOT basic color keywords?",
+answer: "Olive",
+options: [
+    "Olive",
+    "White",
+    "Fuchsia",
+    "Navy"
+ ]
+},
+{ 
+    numb: 7,
+question: "Which of the following property specifies a shorthand property for setting the margin properties in one declaration?",
+answer: "Margin",
+options: [
+    "Margin-top",
+    "Border",
+    "Margin-left",
+    "Margin-bottom"
+   ]
+  },
+];
